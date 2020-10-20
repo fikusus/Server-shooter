c455b08219b4512342ff0593b79e095eaa8658a1
Danila Kuzmuk <Da.Kuzmuk@ssu.edu.ua>
2020-10-20 21:41:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,100 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+  setCords,
+  users,
+} = require("./users");
+const { Z_ASCII } = require("zlib");
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require("constants");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Server started</h1>");
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", async ({ name, room }) => {
+    console.log(name + " joined to " + room);
+
+    let x = Math.random() * 10;
+    socket.emit("display-room", {
+      posx: x.toString(),
+    });
+    socket.broadcast.to(room).emit("enter-new-player", {
+      id: socket.id,
+      posx: x.toString(),
+    });
+
+    getUsersInRoom(room).forEach((element) => {
+      socket.emit("enter-new-player", {
+        id: element.id,
+        posx: element.px.toString(),
+      });
+      socket.emit("update-other-player-animator", element.animation);
+
+    });
+
+    socket.join(room);
+    addUser({ id: socket.id, name, room: room, px: x, py: 0, pz: -29 });
+    getUser(socket.id).animation["id"] = socket.id; 
+  });
+
+  socket.on("update-user-animation", async (jsonObj) => {
+    let curUser = getUser(socket.id);
+    jsonObj["id"] = curUser.id;
+    for(var attributename in jsonObj){
+      curUser.animation[attributename] = jsonObj[attributename];
+  }
+    socket.broadcast
+      .to(curUser.room)
+      .emit("update-other-player-animator", jsonObj);
+  });
+
+  socket.on("send-player-cords", async ({ px, py, pz, rx, ry, rz }) => {
+    let curUser = getUser(socket.id);
+    setCords(socket.id, px, py, pz, rx, ry, rz);
+    socket.broadcast.to(curUser.room).emit("update-users-cords", {
+      id: socket.id,
+      posx: px.toString(),
+      posy: py.toString(),
+      posz: pz.toString(),
+      rotx: rx.toString(),
+      roty: ry.toString(),
+      rotz: rz.toString(),
+    });
+  });
+
+  socket.on("send-spine-cords", async ({rx, ry, rz }) => {
+    let curUser = getUser(socket.id);
+    console.log(rx + " " + ry + " " + rz);
+    socket.broadcast.to(curUser.room).emit("update-spine-cords", {
+      id: socket.id,
+      rotx: rx.toString(),
+      roty: ry.toString(),
+      rotz: rz.toString(),
+    });
+  });
+
+
+  socket.on("disconnect", async () => {
+    let curUser = getUser(socket.id);
+    console.log(curUser.name + " leave the " + curUser.room);
+    await socket.broadcast
+      .to(curUser.room)
+      .emit("disconnect-from-room", { id: socket.id });
+    removeUser(socket.id);
+  });
+});
+
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started.`)
+);
---
+++ users.js
@@ -0,0 +1,37 @@
+const users = [];
+
+const addUser = ({ id, name, room, px, py, pz }) => {
+  const user = { id, name, room, px, py, pz, rx: 0, ry: 0, rz: 0, animation:new Object()};
+  users.push(user);
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+
+  if (index !== -1) return users.splice(index, 1)[0];
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const setCords = (id, npx, npy, npz, nrx, nry, nrz) => {
+  let curr = getUser(id);
+  curr.px = npx;
+  curr.py = npy;
+  curr.pz = npz;
+
+  curr.rx = nrx;
+  curr.ry = nry;
+  curr.rz = nrz;
+};
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+  setCords,
+  users,
+};
