c5997028d45e2b3d69de705cb8580f97ad507b9d
Danila Kuzmuk <Da.Kuzmuk@ssu.edu.ua>
2020-11-15 15:07:50 +0200

    multy test

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // Server.js (упрощённый)
 var cluster = require('cluster');
+
+var rtg   = require("url").parse("redis://redistogo:57b9be5a47c379bb83372851baead95a@soapfish.redistogo.com:10827/");
+
 var config = {
     numWorkers: require('os').cpus().length,
 };
+var server = require('http').createServer();
+var io = require('socket.io').listen(server);
+
+const redis = require('redis');
+const redisAdapter = require('socket.io-redis');
+
+const pubClient = redis.createClient(rtg.port, rtg.hostname, { auth_pass: "57b9be5a47c379bb83372851baead95a" });
+const subClient = pubClient.duplicate();
+io.adapter(redisAdapter({ pubClient, subClient }));
+
+
 
 cluster.setupMaster({
     exec: "worker.js"
